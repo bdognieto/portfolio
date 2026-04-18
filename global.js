7340d15c0880a7b2b038358601ed6bdbf3ee6e71
bdognieto <bnieto@ucsd.edu>
2026-04-17 18:37:25 -0700

    add automatic page link

--- global.js
+++ global.js
@@ -3,3 +3,15 @@ console.log('IT’S ALIVE!');
 function $$(selector, context = document) {
   return Array.from(context.querySelectorAll(selector));
 }
+// get all nav links
+let navLinks = $$("nav a");
+
+// find the link that matches the current page
+let currentLink = navLinks.find(
+  (a) => a.host === location.host && a.pathname === location.pathname
+);
+
+// add 'current' class if found
+if (currentLink) {
+  currentLink.classList.add("current");
+}
