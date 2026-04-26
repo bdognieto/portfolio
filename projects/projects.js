8b7348b7bb277ef2fb60c1a537e836261dd1ca5d
bdognieto <bnieto@ucsd.edu>
2026-04-25 18:38:14 -0700

    add project count

--- projects/projects.js
+++ projects/projects.js
@@ -3,5 +3,9 @@ import { fetchJSON, renderProjects } from '../global.js';
 const projects = await fetchJSON('../lib/projects.json');
 
 const projectsContainer = document.querySelector('.projects');
+const title = document.querySelector('.projects-title');
 
 renderProjects(projects, projectsContainer, 'h2');
+
+const count = projects.length;
+title.textContent = `Projects (${count})`;
