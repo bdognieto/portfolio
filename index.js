af72928b7017342b6d419aa0a3fff615fbe77bca
bdognieto <bnieto@ucsd.edu>
2026-04-25 20:44:18 -0700

    add profile stats and api

--- index.js
+++ index.js
@@ -1,13 +1,22 @@
-import { fetchJSON, renderProjects } from './global.js';
+import { fetchJSON, renderProjects, fetchGithubData } from './global.js';
 
-// get ALL projects
 const projects = await fetchJSON('./lib/projects.json');
-
-// get first 3
 const latestProjects = projects.slice(0, 3);
 
-// find container on homepage
 const projectsContainer = document.querySelector('.projects');
-
-// render them
 renderProjects(latestProjects, projectsContainer, 'h2');
+
+const githubData = await fetchGithubData('bdognieto');
+
+const profileStats = document.querySelector('#profile-stats');
+
+if (profileStats) {
+  profileStats.innerHTML = `
+    <dl>
+      <dt>Public Repos:</dt><dd>${githubData.public_repos}</dd>
+      <dt>Public Gists:</dt><dd>${githubData.public_gists}</dd>
+      <dt>Followers:</dt><dd>${githubData.followers}</dd>
+      <dt>Following:</dt><dd>${githubData.following}</dd>
+    </dl>
+  `;
+}
