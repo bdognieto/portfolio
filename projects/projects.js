b70c16419c35ac14234a8948e255e8694b40453e
bdognieto <bnieto@ucsd.edu>
2026-05-01 15:19:14 -0700

    add project d3 pie chart

--- projects/projects.js
+++ projects/projects.js
@@ -1,3 +1,4 @@
+import * as d3 from 'https://cdn.jsdelivr.net/npm/d3@7.9.0/+esm';
 import { fetchJSON, renderProjects } from '../global.js';
 
 const projects = await fetchJSON('../lib/projects.json');
@@ -9,3 +10,24 @@ renderProjects(projects, projectsContainer, 'h2');
 
 const count = projects.length;
 title.textContent = `Projects (${count})`;
+
+// Pie chart code starts here
+let data = [1, 2];
+
+let arcGenerator = d3.arc()
+  .innerRadius(0)
+  .outerRadius(50);
+
+let sliceGenerator = d3.pie();
+let arcData = sliceGenerator(data);
+
+let arcs = arcData.map((d) => arcGenerator(d));
+
+let colors = ['gold', 'purple'];
+
+arcs.forEach((arc, idx) => {
+  d3.select('#projects-pie-plot')
+    .append('path')
+    .attr('d', arc)
+    .attr('fill', colors[idx]);
+});
