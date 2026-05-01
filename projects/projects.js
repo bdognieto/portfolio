d1f2c74e978b263e2851aa310a908493d0a2fd43
bdognieto <bnieto@ucsd.edu>
2026-05-01 15:38:18 -0700

    add legend

--- projects/projects.js
+++ projects/projects.js
@@ -12,13 +12,20 @@ const count = projects.length;
 title.textContent = `Projects (${count})`;
 
 // Pie chart code starts here
-let data = [1, 2, 3, 4, 5, 5];
+let data = [
+  { value: 1, label: 'apples' },
+  { value: 2, label: 'oranges' },
+  { value: 3, label: 'mangos' },
+  { value: 4, label: 'pears' },
+  { value: 5, label: 'limes' },
+  { value: 5, label: 'cherries' },
+];
 
 let arcGenerator = d3.arc()
   .innerRadius(0)
   .outerRadius(50);
 
-let sliceGenerator = d3.pie();
+let sliceGenerator = d3.pie().value(d => d.value);
 let arcData = sliceGenerator(data);
 
 let arcs = arcData.map((d) => arcGenerator(d));
@@ -30,4 +37,13 @@ arcs.forEach((arc, idx) => {
     .append('path')
     .attr('d', arc)
     .attr('fill', colors(idx));
+});
+
+let legend = d3.select('.legend');
+
+data.forEach((d, idx) => {
+  legend
+    .append('li')
+    .attr('style', `--color:${colors(idx)}`)
+    .html(`<span class="swatch"></span> ${d.label} <em>(${d.value})</em>`);
 });
