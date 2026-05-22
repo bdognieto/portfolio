cc85409354c4dbbcbef5ef368437bde4fcb1da38
bdognieto <bnieto@ucsd.edu>
2026-05-22 14:23:08 -0700

    stable circles and transitions

--- meta/main.js
+++ meta/main.js
@@ -153,7 +153,7 @@ function renderScatterPlot(data, commits) {
 
   dots
     .selectAll('circle')
-    .data(sortedCommits)
+    .data(sortedCommits, d => d.id)
     .join('circle')
     .attr('cx', d => xScale(d.datetime))
     .attr('cy', d => yScale(d.hourFrac))
@@ -309,7 +309,7 @@ function updateScatterPlot(data, commits) {
 
   dots
     .selectAll('circle')
-    .data(sortedCommits)
+    .data(sortedCommits, d => d.id)
     .join('circle')
     .attr('cx', d => xScale(d.datetime))
     .attr('cy', d => yScale(d.hourFrac))
